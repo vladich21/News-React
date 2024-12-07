fac4a2b0955c7077e543f30305edbdf9993c44e6
Vladislav <vladkoval039@gmail.com>
2024-12-07 19:10:12 +0300

    add latest news

--- src/Api/apiNews.js
+++ src/Api/apiNews.js
@@ -27,6 +27,20 @@ keywords,
 	}
 }
 
+export const getLatestNews = async ()=> {
+	try {
+		const response = await axios.get(`${BASE_URL}latest-news`, {params: {
+			apiKey:API_KEY,
+			
+		}
+	})
+	return response.data;
+	} catch (error) {
+		console.log(error);
+		
+	}
+}
+
 export const getCategories = async ()=> {
 	try {
 		const response = await axios.get(`${BASE_URL}available/categories`, {params: {
--- src/components/NewsByFilters/NewsByFilters.jsx
+++ src/components/NewsByFilters/NewsByFilters.jsx
@@ -1,11 +1,29 @@
-import { TOTAL_PAGES } from '../../constant/constant';
+import { PAGE_SIZE, TOTAL_PAGES } from '../../constant/constant';
 import styles from './style.module.css'
-import Pagination from '../Pagination/Pagination'
 import NewsList from '../NewsList/NewsList'
 import NewsFilters from '../NewsFilters/NewsFilters';
+import { useFilters } from '../../helpers/hooks/useFilters';
+import { useDebounce } from '../../helpers/hooks/useDebounce';
+import { useFetch } from '../../helpers/hooks/useFetch';
+import { getNews } from '../../Api/apiNews';
+import PaginationWrapper from '../PaginationWrapper/PaginationWrapper';
 
 
-const NewsByFilters = ({filters, changeFilter, isLoading, news}) =>{
+const NewsByFilters = () =>{
+
+	const {filters, changeFilter} = useFilters({
+		page_number: 1,
+		page_size: PAGE_SIZE,
+		category: null, 
+		keywords: '',
+	})
+	
+	const debouncedKeywords = useDebounce(filters.keywords, 1500)
+	
+	const {data, isLoading} = useFetch(getNews, {
+		...filters,
+		keywords: debouncedKeywords,
+	})
 
 const handleNextPage = () => {
 	if (filters.page_number < TOTAL_PAGES) {
@@ -26,20 +44,13 @@ const handePageClick = (pageNumber) => {
 	return(
 		<section className={styles.section}>
 			<NewsFilters filters={filters} changeFilter={changeFilter}/>
-
-		<Pagination 
-		handleNextPage={handleNextPage}  
+		
+<PaginationWrapper top bottom	handleNextPage={handleNextPage}  
 		handlePreviousPage={handlePreviousPage} handePageClick={handePageClick} 
 		totalPages={TOTAL_PAGES} 
-		currentPage={filters.page_number}/>
-
-	<NewsList news={news} isLoading={isLoading}/>
-	
-	<Pagination 
-		handleNextPage={handleNextPage}  
-		handlePreviousPage={handlePreviousPage} handePageClick={handePageClick} 
-		totalPages={TOTAL_PAGES} 
-		currentPage={filters.page_number}/>
+		currentPage={filters.page_number}>
+	<NewsList news={data?.news} isLoading={isLoading}/>
+	</PaginationWrapper >
 		</section>
 	)
 }
